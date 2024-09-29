661045a5d4349f5a3fdef9d7c4540c57f63b537d
JayeMcCracken <jm@occam.global>
2024-09-29 15:25:31 +0800

    Add readme handling on unauthed homepage

--- README.md
+++ README.md
@@ -1,8 +1,16 @@
+I use an ubuntu dockerised dev environment to keep the experience consistent across machines.
+- To run, open docker desktop and vscode
+- In vscode hit F1 and select "Dev containers: Rebuild and Reopen in Container" to spin up a local dev environment running the project here: http://localhost:8000
+# I need to update this to use the default ubuntu image at base
+I've opted to use swagger via external definitions rendered here: http://localhost:8000/swagger-ui/index.html#/
+
 Notes:
+
 In PHP files I've marked any meta code assessment specific comments with a "#"
+- If you search for "# " and filter for only .php files you will find my reasoning behind including features
 Standard comments have been made with "//"
 
-Front end:
+Front end setup history:
 ```
 php artisan ui react --auth
 ```
--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -4,6 +4,10 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Models\Post;
+use League\CommonMark\MarkdownConverter;
+use League\CommonMark\Extension\CommonMark\CommonMarkCoreExtension;
+use League\CommonMark\Environment\Environment;
+use League\CommonMark\Extension\GithubFlavoredMarkdownExtension;
 
 class HomeController extends Controller
 {
@@ -14,7 +18,32 @@ class HomeController extends Controller
      */
     public function __construct()
     {
-        $this->middleware('auth');
+        $this->middleware('auth')->except('welcome');
+    }
+
+    /**
+     * Show the guest homepage containing the project readme
+     *
+     * @return \Illuminate\Contracts\Support\Renderable
+     */
+    public function welcome()
+    {
+        // Load the README file
+        $readmePath = base_path('README.md');
+        $readme = file_exists($readmePath) ? file_get_contents($readmePath) : 'README file not found.';
+
+        // Set up the Markdown environment
+        $environment = new Environment([]);
+        $environment->addExtension(new CommonMarkCoreExtension());
+        $environment->addExtension(new GithubFlavoredMarkdownExtension());
+
+        // Instantiate the MarkdownConverter with the environment
+        $converter = new MarkdownConverter($environment);
+
+        // Convert Markdown to HTML using the new method
+        $readmeHtml = $converter->convert($readme)->getContent();
+
+        return view('welcome', ['readme' => $readmeHtml]);
     }
 
     /**
